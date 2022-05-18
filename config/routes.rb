8c0a2d9d30895246dc57887c15b4c2960949f272
vitaliy <vitaliy.skoryy@googlemail.com>
2022-05-18 16:40:27 +0200

    getting the hand of things

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # A restaurant must have a name, an address and a category.
+  # A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
+  # When a restaurant is destroyed, all of its reviews must be destroyed as well.
+  # A review must belong to a restaurant.
+  # A review must have content and a rating.
+  # A review’s rating must be a number between 0 and 5.
+  get 'name', to: 'pages#name'
 end
